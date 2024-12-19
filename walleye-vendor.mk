a51ea30337b8aee537579ff8c7ab6920a83f9f49
Nolen Johnson <johnsonnolen@gmail.com>
2024-12-19 13:14:10 -0500

    Expose libhdrplusclient to build system
    * Fixed building camera HAL after
    * 15QPR1 dropped hardware/google/easel
    
    Change-Id: I5cb0d5dac80fb66179eec31c9216f1303aefe3cf

--- walleye-vendor.mk
+++ walleye-vendor.mk
@@ -604,7 +604,6 @@ PRODUCT_COPY_FILES += \
     vendor/google/walleye/proprietary/vendor/lib/libgpustats.so:$(TARGET_COPY_OUT_VENDOR)/lib/libgpustats.so \
     vendor/google/walleye/proprietary/vendor/lib/libgsl.so:$(TARGET_COPY_OUT_VENDOR)/lib/libgsl.so \
     vendor/google/walleye/proprietary/vendor/lib/libhalide_hexagon_host.so:$(TARGET_COPY_OUT_VENDOR)/lib/libhalide_hexagon_host.so \
-    vendor/google/walleye/proprietary/vendor/lib/libhdrplusclient.so:$(TARGET_COPY_OUT_VENDOR)/lib/libhdrplusclient.so \
     vendor/google/walleye/proprietary/vendor/lib/libhdrplusclientimpl.so:$(TARGET_COPY_OUT_VENDOR)/lib/libhdrplusclientimpl.so \
     vendor/google/walleye/proprietary/vendor/lib/libhdrplusmessenger.so:$(TARGET_COPY_OUT_VENDOR)/lib/libhdrplusmessenger.so \
     vendor/google/walleye/proprietary/vendor/lib/libidl.so:$(TARGET_COPY_OUT_VENDOR)/lib/libidl.so \
@@ -969,6 +968,7 @@ PRODUCT_COPY_FILES += \
     vendor/google/walleye/proprietary/vendor/radio/qcril_database/upgrade/9_remove_germany_ecc_110.sql:$(TARGET_COPY_OUT_VENDOR)/radio/qcril_database/upgrade/9_remove_germany_ecc_110.sql
 
 PRODUCT_PACKAGES += \
+    libhdrplusclient \
     libsdsprpc \
     ModemService \
     QtiTelephonyService \
